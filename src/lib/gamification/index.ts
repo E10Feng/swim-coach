6d39e1c6c7f54b305baf43228d5ece4c6ab8d6d0
Ethan Burr <ethan.burr@gmail.com>
2026-05-29 13:03:54 -0400

    chore: add gamification barrel export

---
+++ src/lib/gamification/index.ts
@@ -0,0 +1,4 @@
+export { calculateXP, calculateLevel, getNextLevel, LEVELS } from './xp'
+export { updateStreak } from './streaks'
+export { checkAndAwardBadges, BADGE_SLUGS } from './badges'
+export { getDashboardMessage } from './coach-messages'
